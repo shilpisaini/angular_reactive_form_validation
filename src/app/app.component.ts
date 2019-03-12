269f5d907148bb9d138396c7899452c9db68f67e
shilpisaini <shilpisaini99@gmail.com>
2019-03-12 22:28:42 +0530

    Reactive Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import  { FormBuilder, FormGroup , Validators} from '@angular/forms';
+import { MustMatch } from './mustmatch';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-reactive-form';
+export class AppComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted =false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+  },{
+    validator: MustMatch('password', 'confirmPassword')
+});
+
+  }
+
+  get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+        if (this.registerForm.invalid) { //process will stop here if form is invalid
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
 }
